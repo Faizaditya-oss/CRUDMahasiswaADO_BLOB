11c6ac5de1d8b4607825f4b37a1a6975e90b985d
Faizaditya-oss <faizaditya750@gmail.com>
2026-06-21 18:41:27 +0700

    Menambahkan kode untuk button import database

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -68,7 +68,7 @@
             this.lblCountMhs = new System.Windows.Forms.TextBox();
             this.btnRekap = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnImpExcel = new System.Windows.Forms.Button();
             this.btnImpDB = new System.Windows.Forms.Button();
             this.fotoMhs = new System.Windows.Forms.PictureBox();
             this.btnCari = new System.Windows.Forms.Button();
@@ -444,14 +444,15 @@
             this.button3.Text = "Refresh";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnImpExcel
             // 
-            this.button4.Location = new System.Drawing.Point(857, 172);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(88, 61);
-            this.button4.TabIndex = 25;
-            this.button4.Text = "Import from Excel";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnImpExcel.Location = new System.Drawing.Point(857, 172);
+            this.btnImpExcel.Name = "btnImpExcel";
+            this.btnImpExcel.Size = new System.Drawing.Size(88, 61);
+            this.btnImpExcel.TabIndex = 25;
+            this.btnImpExcel.Text = "Import from Excel";
+            this.btnImpExcel.UseVisualStyleBackColor = true;
+            this.btnImpExcel.Click += new System.EventHandler(this.btnImpExcel_Click);
             // 
             // btnImpDB
             // 
@@ -461,6 +462,7 @@
             this.btnImpDB.TabIndex = 26;
             this.btnImpDB.Text = "Import to Database";
             this.btnImpDB.UseVisualStyleBackColor = true;
+            this.btnImpDB.Click += new System.EventHandler(this.btnImpDB_Click);
             // 
             // fotoMhs
             // 
@@ -498,7 +500,7 @@
             this.Controls.Add(this.btnCari);
             this.Controls.Add(this.fotoMhs);
             this.Controls.Add(this.btnImpDB);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnImpExcel);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.btnRekap);
             this.Controls.Add(this.lblCountMhs);
@@ -580,7 +582,7 @@
         private System.Windows.Forms.TextBox lblCountMhs;
         private System.Windows.Forms.Button btnRekap;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnImpExcel;
         private System.Windows.Forms.Button btnImpDB;
         private System.Windows.Forms.PictureBox fotoMhs;
         private System.Windows.Forms.Button btnCari;
